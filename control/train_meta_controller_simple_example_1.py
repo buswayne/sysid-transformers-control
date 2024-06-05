0bb35066c8493482c230b27f0a9a3bc59791d60b
PaoloPiccirilli <149780385+PaoloPiccirilli@users.noreply.github.com>
2024-06-05 19:23:01 +0200

    test open loop, error added test closed loop, various references added, error metrics added, needs to be discussed

--- control/train_meta_controller_simple_example_1.py
+++ control/train_meta_controller_simple_example_1.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                         help='Saved model name')
     parser.add_argument('--in-file', type=str, default="ckpt_controller_simple_example_1", metavar='S',
                         help='Loaded model name (when resuming)')
-    parser.add_argument('--init-from', type=str, default="resume", metavar='S',
+    parser.add_argument('--init-from', type=str, default="scratch", metavar='S',
                         help='Init from (scratch|resume|pretrained)')
     parser.add_argument('--seed', type=int, default=42, metavar='N',
                         help='Seed for random number generation')
